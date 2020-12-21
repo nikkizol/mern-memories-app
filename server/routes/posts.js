98de3b2db6d54f15c12e7586c686b461f076f872
MZ <nikita.zholkovskyi@gmail.com>
2020-12-21 15:02:44 +0100

    created components

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,3 @@
-import PostMessage from "../models/postMessage.js";
-
 import express from "express";
 import { getPosts, createPost } from "../controllers/posts.js";
 const router = express.Router();
