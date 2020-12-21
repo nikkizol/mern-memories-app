b6c3d35e06577074ddad3ebac366f404d4019ec7
MZ <nikita.zholkovskyi@gmail.com>
2020-12-21 13:53:33 +0100

    conncted to db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
 import express from "express";
-import bodyParser from "body-parser ";
+import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// const CONNECTION_URL =
+//   "mongodb+srv://username:password@cluster0.s752d.mongodb.net/MERN?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () =>
+      app /
+      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
